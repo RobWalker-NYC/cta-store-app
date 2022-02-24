f1f5ecd35837ce73575a3494105a917e1390fca7
rob <robwalker@pursuit.org>
2022-02-24 10:49:04 -0500

    pushing for heroku

--- back-end/db/prod_schema.sql
+++ back-end/db/prod_schema.sql
@@ -1,6 +1,16 @@
 DROP TABLE IF EXISTS test;
 
-CREATE TABLE test (
+CREATE TABLE donuts (
     id SERIAL PRIMARY KEY, 
-    name TEXT
+    name TEXT NOT NULL,
+    description TEXT,
+    jolt NUMERIC DEFAULT 1,
+    CHECK (jolt >= 1 AND jolt <= 2),
+    price INTEGER DEFAULT 0,
+    rating NUMERIC DEFAULT 0,
+    CHECK (rating >= 0 AND rating <= 5),
+    featured BOOLEAN,
+    quantity INTEGER DEFAULT 1,
+    image TEXT,
+    responsibility BOOLEAN
 );
